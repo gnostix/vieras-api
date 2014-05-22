c486b8756564b3f14a843f3d84dec29ba2051649
Alex Pappas <apappas@gnostix.gr>
2014-05-22 12:58:09 +0300

    added some extra classes in User.scala

--- src/main/scala/gr/gnostix/api/models/User.scala
+++ src/main/scala/gr/gnostix/api/models/User.scala
@@ -4,12 +4,15 @@ import java.sql.Date
 
 case class User(name: String)
 case class Customer(id: Int, name: String, lastname: String, t_results: Int)
+case class CustAddress(streetAddress: String, StreetNum: Int, postalCode: Int, city: String)
+case class Cust
 
 
 /*
 case class User1(userId: Int, username: String, password: String, userLevel: Int, firstname: String, lastname: String,
-          registrationDate: Date, email: String, streetAddress: String, StreetNum: Int, postalCode: Int, city: String,
-          company: String, language: String, expirationDate: Date, totalCounts: Int, totalKeywords: Int, enabled: Int,
-          sentEmail: Int, totalProfiles: Int, totalFbFanPages: Int, totalTwitterAccounts: Int, totalTopicProfiles: String)
+                 registrationDate: Date, email: String, streetAddress: String, StreetNum: Int, postalCode: Int, city: String,
+                 company: String, language: String, expirationDate: Date, totalCounts: Int, totalKeywords: Int, enabled: Int,
+                 sentEmail: Int, totalProfiles: Int, totalFbFanPages: Int, totalTwitterAccounts: Int, totalTopicProfiles: String)
 */
+
 
