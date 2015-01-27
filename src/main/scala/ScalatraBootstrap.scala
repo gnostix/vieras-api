999f80009ccdcb33bca8efa54421ffecbb73d101
Alex Pappas <apappas@gnostix.gr>
2015-01-27 20:04:41 +0200

    plumbing for report added..

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -69,6 +69,9 @@ class ScalatraBootstrap extends LifeCycle {
     context.mount(new YoutubeDashboardServlet(executor), "/api/user/socialchannels/dashboard/youtube/*")
     context.mount(new HospitalityDashboardServlet(executor), "/api/user/socialchannels/dashboard/hotel/*")
 
+    context.mount(new ReportServlet(executor), "/api/user/dashboard/report/*")
+
+
   }
 
   //
