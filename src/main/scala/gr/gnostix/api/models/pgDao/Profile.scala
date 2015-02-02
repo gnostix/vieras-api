6826755d11b21e7448d8da9fb318f4fc4440daeb
Alex Pappas <apappas@gnostix.gr>
2015-02-02 15:15:55 +0200

    Profiles api change

--- src/main/scala/gr/gnostix/api/models/pgDao/Profile.scala
+++ src/main/scala/gr/gnostix/api/models/pgDao/Profile.scala
@@ -90,7 +90,7 @@ object ProfileDao extends DatabaseAccessSupportPg {
           }
 
           if (profiles.size > 0) {
-            Some(ApiData("profile", profiles.head))
+            Some(ApiData("profile", profiles))
           } else Some(ApiData("nodata", profiles))
 
         } catch {
