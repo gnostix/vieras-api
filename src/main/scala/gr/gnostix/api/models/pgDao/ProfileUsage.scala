caffc8a017708cd4df6b5e8b05781755e5e35c89
Alex Pappas <apappas@gnostix.gr>
2015-05-04 10:02:43 +0300

    chenge in profiles

--- src/main/scala/gr/gnostix/api/models/pgDao/ProfileUsage.scala
+++ src/main/scala/gr/gnostix/api/models/pgDao/ProfileUsage.scala
@@ -8,22 +8,22 @@ import scala.slick.jdbc.{GetResult, StaticQuery => Q}
 /**
  * Created by rebel on 12/6/14.
  */
-case class ProfilesUsage(id: Int, userLevel: Int, maxCounts: Int, description: String, maxKeywords: Int,
+case class ProfilesUsage(userLevel: Int, maxCounts: Int, description: String, maxKeywords: Int,
          maxTopics: Int, socialAccounts: Int, maxProfiles: Int, companies: Int, companyUrls: Int, competitorUrls: Int)
 
 
 object ProfilesUsage extends DatabaseAccessSupportPg {
 
   implicit val getProfilesUsageResult = GetResult(r => ProfilesUsage(r.<<, r.<<, r.<<, r.<<,
-    r.<<, r.<<, r.<<, r.<<, r.<<, r.<<, r.<<))
+    r.<<, r.<<, r.<<, r.<<, r.<<, r.<<))
 
   def findByUserlevel(userLevel: Int) = {
     getConnection withSession {
       implicit session =>
         val records = Q.queryNA[ProfilesUsage](s"""
-          select ID,USER_LEVEL,MAX_COUNTS,DESCRIPTION,MAX_KEYWORDS,MAX_TOPICS,
+          select ID,MAX_COUNTS,DESCRIPTION,MAX_KEYWORDS,MAX_TOPICS,
           social_accounts,profiles,companies,company_urls,competitor_urls
-            from vieras.user_level_counts where user_level = $userLevel
+            from vieras.user_level_counts where id = $userLevel
           """)
         records.first
     }
